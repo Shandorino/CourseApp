f5639e0e82d108ed8f4f0985ebdc24c706edcb1c
Shandorino <slava.ermakov111@gmail.com>
2022-05-25 19:23:28 +0500

    Camera commit

--- app/src/main/java/com/odsstudio/courseapp/MainActivity.kt
+++ app/src/main/java/com/odsstudio/courseapp/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.odsstudio.courseapp
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaParser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private val cameraButtonListener = View.OnClickListener {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                1
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.cameraPermission)
+        button.setOnClickListener(cameraButtonListener)
+
+
     }
 }
